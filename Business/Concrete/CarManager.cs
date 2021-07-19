07a052cca17322487c273014f8e300ea3d917b13
MUHAMMED BURAK ALTUN <MALTUN15@ku.edu.tr>
2021-07-19 21:13:07 +0300

    15 - Enterprise Software Architerctures - AOP

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Results;
@@ -23,17 +24,20 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        [CacheAspect(duration:10)]
         public IDataResult<List<Car>> GetAll()
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll());
         }
 
-
+        [CacheAspect(duration: 10)]
         public IDataResult<Car> GetById(int Id)
         {
             return new SuccessDataResult<Car>(_carDal.Get(c => c.Id == Id));
         }
 
+        [CacheRemoveAspect("ICarService.Get")]
         [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {            
@@ -41,6 +45,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.CarAdded);
         }
 
+        [CacheRemoveAspect("ICarService.Get")]
         [ValidationAspect(typeof(CarValidator))]
         public IResult Update(Car car)
         {
@@ -57,27 +62,32 @@ namespace Business.Concrete
             return new SuccessResult(Messages.CarUpdated);
         }
 
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Delete(Car car)
         {
             _carDal.Delete(car);
             return new SuccessResult(Messages.CarDeleted);
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Car>> GetByBrandId(int id)
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == id),"Cars are fecthed by brand id");
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<Car>> GetByColorId(int id)
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == id), "Cars are fecthed by color id");
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<CarDetailDTO> GetDetailsById(int id)
         {
             return new SuccessDataResult<CarDetailDTO>(_carDal.GetDetailsById(id));
         }
 
+        [CacheAspect(duration: 10)]
         public IDataResult<List<CarDetailDTO>> GetAllDetails()
         {
             return new SuccessDataResult<List<CarDetailDTO>>(_carDal.GetAllDetails());
